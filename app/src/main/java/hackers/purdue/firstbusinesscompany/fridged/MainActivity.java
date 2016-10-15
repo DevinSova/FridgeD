07c37cad996557f23e6cd92b49798bac67bbe592
DevinSova <dsova29@gmail.com>
2016-10-15 15:55:39 -0400

    Shared Preferences half done (To store list elements)

--- app/src/main/java/hackers/purdue/firstbusinesscompany/fridged/MainActivity.java
+++ app/src/main/java/hackers/purdue/firstbusinesscompany/fridged/MainActivity.java
@@ -1,15 +1,33 @@
 package hackers.purdue.firstbusinesscompany.fridged;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ListViewCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ListView fridgeItems;
+    Adapter adapter;
+    SharedPreferences fridgeSharedPreferences;
+    String[] fridgeArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +35,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        fridgeSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        if (fridgeSharedPreferences.contains("fridges")) {
+            fridgeArray = fridgeSharedPreferences.getString("fridges", "").split(",");
+        }
+        fridgeItems = (ListView) findViewById(R.id.fridgeSharedPreferences);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +75,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void save() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < fridgeArray.length; i++) {
+            builder.append(fridgeArray[i]).append(",");
+        }
+        fridgeSharedPreferences.edit().putString("fridges", builder.toString());
     }
 }
