5ae1d38abed8232f05196147b4d56f73eb7faa82
jaeyeonkim0919 <jaeyeonkim0919@gmail.com>
2016-10-15 16:04:56 -0400

    Implemented arrayListMethods -complete

--- app/src/main/java/hackers/purdue/firstbusinesscompany/fridged/arrayListFunctions.java
+++ app/src/main/java/hackers/purdue/firstbusinesscompany/fridged/arrayListFunctions.java
@@ -1,22 +1,18 @@
 package hackers.purdue.firstbusinesscompany.fridged;
 import java.util.ArrayList;
 
-/**
- * Created by kimeric on 2016. 10. 15..
- */
-
 public class arrayListFunctions {
 
-    public static ArrayList<String> addItem (ArrayList<String> foodItems, String foodItem){
-        foodItems.add(foodItem);
-        return foodItems;
+    public static ArrayList<String> addItem (ArrayList<String> Items, String Item){
+        Items.add(Item);
+        return Items;
     }
-    public static boolean searchItem (ArrayList<String> foodItems, String foodItem){
-        return foodItems.contains(foodItem);
+    public static boolean searchItem (ArrayList<String> Items, String Item){
+        return Items.contains(Item);
     }
-    public static boolean deleteItem (ArrayList<String> foodItems, String foodItem){
-        if(foodItems.contains(foodItems)){
-            foodItems.remove(foodItem.indexOf(foodItem));
+    public static boolean deleteItem (ArrayList<String> Items, String Item){
+        if(Items.contains(Items)){
+            Items.remove(Item.indexOf(Item));
             return true;
         }
         else{
@@ -24,17 +20,37 @@ public class arrayListFunctions {
         }
 
     }
-    public static ArrayList<String> sortArrayList (ArrayList<String> foodItems, char ch){
+    public static ArrayList<String> sortArrayList (ArrayList<String> Items, char ch){
 
         if(ch == 'A'){              //Ascending Order
-
+            for(int i=0; i<(Items.size()-1); i++){
+                if((Items.get(i).compareToIgnoreCase(Items.get(i+1))) > 0){
+                    String temp = Items.get(i);
+                    Items.set(i, Items.get(i+1));
+                    Items.set((i+1), temp);
+                }
+                else{
+                    continue;
+                }
+            }
         }
         else if(ch == 'D'){         //Descending Order
+            for(int i=0; i<(Items.size()-1); i++){
+                if((Items.get(i).compareToIgnoreCase(Items.get(i+1))) < 0){
+                    String temp = Items.get(i);
+                    Items.set(i, Items.get(i+1));
+                    Items.set((i+1), temp);
+                }
+                else{
+                    continue;
+                }
+            }
         }
         else{
             System.out.println("Wrong Input");
-            return foodItems;
+            return Items;
         }
+    return Items;
     }
 
 
