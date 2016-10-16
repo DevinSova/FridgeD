e80bf92d50a3a5f5d39aaf00d0764aba9a2cd851
Ben Scholer <benscholer3248511@gmail.com>
2016-10-15 22:53:58 -0400

    I'm done if everything works perfectly...

--- app/src/main/java/hackers/purdue/firstbusinesscompany/fridged/RecipeActivity.java
+++ app/src/main/java/hackers/purdue/firstbusinesscompany/fridged/RecipeActivity.java
@@ -1,13 +1,38 @@
 package hackers.purdue.firstbusinesscompany.fridged;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class RecipeActivity extends AppCompatActivity {
+
+    ArrayList<Ingredient> ingredients;
+    ArrayList<Recipe> recipes;
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+        Bundle extras = getIntent().getExtras();
+
+        list = (ListView) findViewById(R.id.recipieList);
+
+        if(extras != null) {
+            ingredients = (ArrayList<Ingredient>) extras.get("ingredients");
+        } else {
+            ingredients.add(new Ingredient("Chicken"));
+            ingredients.add(new Ingredient("Milk"));
+            ingredients.add(new Ingredient("Cheese"));
+        }
+
+        API api = new API(getApplicationContext(), ingredients);
+        recipes = api.getRecipes();
+        list.setAdapter(new RecipeDisplay(this, recipes));
     }
 }
--- app/src/main/java/hackers/purdue/firstbusinesscompany/fridged/RecipeDisplay.java
+++ app/src/main/java/hackers/purdue/firstbusinesscompany/fridged/RecipeDisplay.java
@@ -1,6 +1,7 @@
 package hackers.purdue.firstbusinesscompany.fridged;
 
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -8,53 +9,88 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RecipeDisplay extends Fragment {
+public class RecipeDisplay extends BaseAdapter {
 
     ImageView image;
     TextView title;
     TextView publisher;
-    Recipe recipe;
+    Context context;
+    ArrayList<Recipe> recipes;
+    private static LayoutInflater inflater = null;
 
-
-    public RecipeDisplay() {
-        // Required empty public constructor
+    public RecipeDisplay(Context context, ArrayList<Recipe> recipes) {
+        this.context = context;
+        this.recipes = recipes;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        View view = inflater.inflate(R.layout.fragment_recipe_display, container, false);
+//
+//        image = (ImageView) view.findViewById(R.id.image);
+//        title = (TextView) view.findViewById(R.id.title);
+//        publisher = (TextView) view.findViewById(R.id.publisher);
+//
+//        image.setImageBitmap(recipe.getImage());
+//        title.setText(recipe.getTitle());
+//        publisher.setText(recipe.getPublisher());
+//
+//        return view;
+//    }
 
-    public Recipe getRecipe() {
-        return this.recipe;
-    }
+
+
+//    public void onClick(View v) {
+//        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(recipe.getUrl()));
+//        startActivity(browserIntent);
+//    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_recipe_display, container, false);
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = convertView;
+        if (view == null)
+            view = inflater.inflate(R.layout.fragment_recipe_display, null);
 
         image = (ImageView) view.findViewById(R.id.image);
         title = (TextView) view.findViewById(R.id.title);
         publisher = (TextView) view.findViewById(R.id.publisher);
 
-        image.setImageBitmap(recipe.getImage());
-        title.setText(recipe.getTitle());
-        publisher.setText(recipe.getPublisher());
+        image.setImageBitmap(recipes.get(position).getImage());
+        title.setText(recipes.get(position).getTitle());
+        publisher.setText(recipes.get(position).getPublisher());
 
         return view;
     }
 
-    public void onClick(View v) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(recipe.getUrl()));
-        startActivity(browserIntent);
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return recipes.size();
     }
 
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return recipes.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
 }
