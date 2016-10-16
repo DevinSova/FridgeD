b98235e63d9af05dce6311a61c07f06faa172920
DevinSova <dsova29@gmail.com>
2016-10-16 03:40:32 -0400

    Remove Item by longpressing. Keyboard automatically comes up on addItemScreen

--- app/src/main/java/hackers/purdue/firstbusinesscompany/fridged/addItem.java
+++ app/src/main/java/hackers/purdue/firstbusinesscompany/fridged/addItem.java
@@ -3,6 +3,7 @@ package hackers.purdue.firstbusinesscompany.fridged;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -19,20 +20,23 @@ public class addItem extends AppCompatActivity
     String newItem;
     Button done;
     Button cancel;
+    InputMethodManager keyboardManager;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.prompt);
         input = (EditText) findViewById(R.id.input);
         done = (Button) findViewById(R.id.donebutton);
+        keyboardManager= (InputMethodManager) getSystemService(getApplicationContext().INPUT_METHOD_SERVICE);
+        keyboardManager.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
 
     }
     public void onDoneClick(View view)
     {
+        keyboardManager.hideSoftInputFromWindow(view.getWindowToken(),0);
         Intent sendIntent = new Intent();
         newItem = input.getText().toString();
         sendIntent.putExtra("addItemString", newItem);
         setResult(RESULT_OK, sendIntent);
         finish();
-
     }
 }
